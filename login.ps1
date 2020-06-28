c512450763bec27982be9309f55651cf9474546d
kgnk-hkr <48877167+kgnk-hkr@users.noreply.github.com>
2020-06-28 12:57:49 +0900

    Update login.ps1

--- login.ps1
+++ login.ps1
@@ -3,7 +3,7 @@ $password = Read-Host "パスワード" -AsSecureString
 
 az login -u $username -p $password
 if (!$?) {
-    Write-Error "Error creating storage account"
+    Write-Error "Error login Azure account"
     exit 2
 }else{
 	exit 0
